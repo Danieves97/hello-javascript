ac2246ea4f229a8e2da4ac023f00419e06e7ad1d
Danieves97 <jdanielglezn@outlook.es>
2024-11-24 21:00:37 -0600

    "Operators exercises"

--- Basic/05-operators-exercises.js
+++ Basic/05-operators-exercises.js
@@ -4,9 +4,37 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=6458
 */
 
 // 1. Crea una variable para cada operación aritmética
+console.log("Operaciones aritméticas")
+let sum = (2 + 4)
+let rest = (10 - 2)
+let mult = (2 * 4)
+let div = (21 / 7)
+let exp = (3 ** 3)
+let mod = (50 % 2)
+
+console.log(sum)
+console.log(rest)
+console.log(mult)
+console.log(div)
+console.log(exp)
+console.log(mod)
 
 // 2. Crea una variable para cada tipo de operación de asignación,
 //    que haga uso de las variables utilizadas para las operaciones aritméticas
+
+console.log("Variables de asignación")
+sum += 1
+rest -= 1
+mult *= 2
+div /= 2
+exp **= 2
+mod %= 1
+console.log(sum)
+console.log(rest)
+console.log(mult)
+console.log(div)
+console.log(exp)
+console.log(mod)
 
 // 3. Imprime 5 comparaciones verdaderas con diferentes operadores de comparación
 
