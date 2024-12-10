a8c25271e12fcfda935619929aac093f56ed40da
Danieves97 <jdanielglezn@outlook.es>
2024-12-10 15:48:59 -0600

    "Doing objects exercises"

--- Basic/19-objects-exercises.js
+++ Basic/19-objects-exercises.js
@@ -5,15 +5,39 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=15675
 
 // 1. Crea un objeto con 3 propiedades
 
+let pet = {
+    name: "koko",
+    age: 7,
+    favFood: "tacos"
+}
+
 // 2. Accede y muestra su valor
+
+console.log(pet.favFood)
 
 // 3. Agrega una nueva propiedad
 
+pet.color = "brown"
+console.log(pet.color)
+
 // 4. Elimina una de las 3 primeras propiedades
+
+delete pet.name
+console.log(pet)
 
 // 5. Agrega una función e invócala
 
+pet.activity = function () {
+    console.log("koko is sleeping")
+}
+
+pet.activity()
+
 // 6. Itera las propiedades del objeto
+
+for(let i in pet){
+    console.log(pet[i])
+}
 
 // 7. Crea un objeto anidado
 
